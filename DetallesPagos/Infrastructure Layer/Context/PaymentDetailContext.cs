4cfb70f1de0955907b4c4edd60035d12ef3cf685
Osia Miguel Clara Reynoso <osiamiguelclarareynoso@gmail.com>
2024-04-03 16:10:42 -0400

    Capa de Infraestructura Terminada

---
+++ DetallesPagos/Infrastructure Layer/Context/PaymentDetailContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PaymentDetails.Infrastructure.Models;
+
+namespace PaymentDetails.Infrastructure.Context
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
